d05491cb4151b1233fa079ee277bbcf5b2c3f2f0
KingNarmar <adlymina99@gmail.com>
2023-09-16 15:02:17 +0300

    create the basic view model class

---
+++ lib/presentation/basic/basic_view_model.dart
@@ -0,0 +1,10 @@
+abstract class BasicViewModel {
+  //Shared variables and functions that will be used through any view model
+}
+
+abstract class BasiceViewModelInput {
+  void strart(); //start view model job
+  void dispose(); //will be called when the view model dies
+}
+
+abstract class BasiceViewModelOutput {}
